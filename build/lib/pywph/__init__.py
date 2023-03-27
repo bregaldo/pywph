355cfbcdab40f912736e82b33783553403203947
Eralys <erwan@allys-fr.org>
2023-03-28 01:57:48 +0200

    remove build from git repo

--- build/lib/pywph/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__all__ = ['filters', 'wph_operator', 'utils', 'wph']
-__version__ = '1.1'
-
-from .filters import *
-from .wph_operator import *
-from .utils import *
-from .wph import *
